1479e9af4024acbf5a46427611b0b4733634bada
Dann7i <hermawanwildan78@gmail.com>
2025-08-18 16:20:40 +0700

    html

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-import http from "http"
-import fs from "fs"
-import path from "path"
+import http from "http";
+import fs from "fs";
+import path from "path";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const hostname = "0.0.0.0";
-const port = 3000
+const port = 3000;
 let extname;
+let filePath;
+let contentType;
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+   filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
   extname = path.extname(filePath);
-  let contentType = "text/html";
+  contentType = "text/html";
   
   switch (extname) {
-    case ".js": contentType = "text/javascript"
-      break;
-      case '.css': contentType = 'text/css'; break;
-    case '.json': contentType = 'application/json'; break;
-    case '.png': contentType = 'image/png'; break;
-    case '.jpg': contentType = 'image/jpg'; break;
+    case ".js": contentType = "text/javascript"; break;
+    case ".css": contentType = "text/css"; break;
+    case ".json": contentType = "application/json"; break;
+    case ".png": contentType = "image/png"; break;
+    case ".jpg": contentType = "image/jpg"; break;
     default:
   }
-});
-fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404 Not Found');
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
+  // fs.readFile 
+  fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+      }
+  });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`'Berjalan di port': ${port} `)
+  console.log(`'Berjalan di port': ${port} `);
 });
