4b2f89d15fed271ef72b87900656dfee42dc56dc
Dann7i <hermawanwildan78@gmail.com>
2025-08-18 16:03:32 +0700

    .

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const __dirname = path.dirname(__filename);
 
 const hostname = "0.0.0.0";
 const port = 3000
-
+let extname;
 const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+  extname = path.extname(filePath);
+  let contentType = "text/html";
   
-})
+  switch (extname) {
+    case ".js": contentType = "text/javascript"
+      break;
+      case '.css': contentType = 'text/css'; break;
+    case '.json': contentType = 'application/json'; break;
+    case '.png': contentType = 'image/png'; break;
+    case '.jpg': contentType = 'image/jpg'; break;
+    default:
+      
+  }
+});
 
 server.listen(port, hostname, () => {
   console.log(`'Berjalan di port': ${port} `)
-})
+});
